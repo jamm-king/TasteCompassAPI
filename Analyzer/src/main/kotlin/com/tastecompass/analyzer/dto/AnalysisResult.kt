06b14f7cece45817b692db8c6266d3343e533abc
jaemin <jmcylove@gmail.com>
2025-05-04 17:36:11 +0900

    Modify: modified AnalysisResult's address to non-nullable filed and menus to List<RestaurantMenu> type #3

--- Analyzer/src/main/kotlin/com/tastecompass/analyzer/dto/AnalysisResult.kt
+++ Analyzer/src/main/kotlin/com/tastecompass/analyzer/dto/AnalysisResult.kt
@@ -1,14 +1,16 @@
 package com.tastecompass.analyzer.dto
 
+import com.tastecompass.domain.entity.RestaurantMenu
+
 data class AnalysisResult(
     val name: String,
     val category: String? = null,
     val phone: String? = null,
-    val address: String? = null,
+    val address: String,
     val businessDays: String? = null,
     val hasWifi: Boolean? = null,
     val hasParking: Boolean? = null,
-    val menus: List<Any>? = null,
+    val menus: List<RestaurantMenu>? = null,
     val minPrice: Int? = null,
     val maxPrice: Int? = null,
     val mood: String,
