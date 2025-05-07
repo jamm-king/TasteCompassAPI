cf25dec61d5c9f65aa4042fda4bbd2e99b61f45a
jaemin <jmcylove@gmail.com>
2025-05-07 17:37:51 +0900

    Add: Created ReviewController that provided api for review crawler #21

--- Application/build.gradle.kts
+++ Application/build.gradle.kts
@@ -4,43 +4,10 @@ plugins {
     id("org.springframework.boot")
 }
 
-//configurations {
-//    compileOnly {
-//        extendsFrom(configurations.annotationProcessor.get())
-//    }
-//}
-
-//extra["springAiVersion"] = "1.0.0-M3"
-
-repositories {
-    mavenCentral()
-//    maven { url = uri("https://repo.spring.io/release") }
-//    maven { url = uri("https://repo.spring.io/snapshot") }
-//    maven { url = uri("https://repo.spring.io/milestone") }
-}
-
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-//    implementation("org.jetbrains.kotlin:kotlin-reflect")
-//    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
-
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation(project(":Controller"))
     implementation(project(":BaseUtil"))
+    implementation(project(":Domain"))
 }
-
-//dependencyManagement {
-//    imports {
-//        mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
-//    }
-//}
-//
-//kotlin {
-//    compilerOptions {
-//        freeCompilerArgs.addAll("-Xjsr305=strict")
-//    }
-//}
