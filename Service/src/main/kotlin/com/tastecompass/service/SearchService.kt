0134f10fe6c7af600b86feaa1df46f2866be4daa
jamm-king <jmcylove@gmail.com>
2025-06-13 17:57:16 +0900

    Refactor: Refactored search method to take weights for embedding as input parameter

--- Service/src/main/kotlin/com/tastecompass/service/SearchService.kt
+++ Service/src/main/kotlin/com/tastecompass/service/SearchService.kt
@@ -3,5 +3,11 @@ package com.tastecompass.service
 import com.tastecompass.domain.entity.Restaurant
 
 interface SearchService {
-    suspend fun search(query: String, topK: Int): List<Restaurant>
+    suspend fun search(
+        query: String,
+        topK: Int,
+        tasteWeight: Float,
+        categoryWeight: Float,
+        moodWeight: Float
+    ): List<Restaurant>
 }
